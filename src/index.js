c81738ac34b834c2bd7541dc3ba2e2ff986cdfe6
DaniMstephens <danimstephens@gmail.com>
2024-01-14 08:58:14 +0000

    Search Bar Javascript function Replace City

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function city(event) {
+  event.preventDefault();
+  let cityTyped = document.querySelector("#enter-city");
+  let h1 = document.querySelector("#city-name-entered");
+  h1.innerHTML = `${cityTyped.value}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", city);
